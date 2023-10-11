aa34725ec3d5b5d685edf5ac9de7de0f32e2d130
Emma Linares <elinares235@gmail.com>
2023-10-11 10:40:29 -0300

    Update Data2

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -490,6 +490,393 @@ export default [
       "sizes": ["6", "7", "8", "9", "10", "11"]
     }
   },
+  {
+    "Id": "Fila001",
+    "name": "Fila Disruptor",
+    "shortDescription": "Diseño audaz y estilo retro.",
+    "description": "Los Fila Disruptor son conocidos por su diseño audaz y su estilo retro. Estos zapatos son perfectos para aquellos que buscan destacar en la moda urbana. Construidos con materiales de alta calidad, brindan comodidad y durabilidad.",
+    "imageUrl": "https://th.bing.com/th/id/OIG.MoYbGzcI5NV9Z._.ff1G?pid=ImgGn",
+    "facts": {
+      "model year": "1996",
+      "material": "Cuero/Sintético",
+      "gender": "Unisex",
+      "colors": ["Blanco", "Negro", "Rosa"],
+      "sizes": ["6", "7", "8", "9", "10", "11"]
+    }
+  },
+  {
+    "Id": "Fila002",
+    "name": "Fila Ray",
+    "shortDescription": "Un estilo retro reinventado.",
+    "description": "Los Fila Ray son una reinterpretación moderna de los icónicos zapatos de los años 90. Con su diseño colorido y llamativo, son ideales para aquellos que aman el estilo retro con un toque contemporáneo.",
+    "imageUrl": "https://th.bing.com/th/id/OIG.IJB6uKNRyzOszeL2prIy?pid=ImgGn",
+    "facts": {
+      "model year": "2018",
+      "material": "Textil/Sintético",
+      "gender": "Mujer",
+      "colors": ["Blanco/Rosa", "Negro/Blanco", "Azul"],
+      "sizes": ["US 5", "US 6", "US 7", "US 8", "US 9"]
+    }
+  },
+  {
+    "Id": "Fila003",
+    "name": "Fila Venom",
+    "shortDescription": "Un clásico de los años 90.",
+    "description": "Los Fila Venom son un clásico de los años 90 que ha regresado con fuerza. Su diseño retro y su construcción de alta calidad los convierten en una opción elegante y cómoda para cualquier ocasión.",
+    "imageUrl": "https://th.bing.com/th/id/OIG.U86x7yqafYLL0j52Srqv?pid=ImgGn",
+    "facts": {
+      "model year": "1994",
+      "material": "Cuero/Sintético",
+      "gender": "Hombre",
+      "colors": ["Blanco/Negro", "Negro/Rojo", "Azul"],
+      "sizes": ["US 7", "US 8", "US 9", "US 10", "US 11"]
+    }
+  },
+  {
+    "Id": "HokaOneOne001",
+    "name": "Hoka One One Clifton 7",
+    "shortDescription": "Comodidad y rendimiento excepcionales.",
+    "description": "El Hoka One One Clifton 7 es conocido por su comodidad excepcional y su capacidad para brindar un alto rendimiento en la carrera. Con su diseño ligero y su amortiguación suave, es una elección popular entre los corredores de larga distancia.",
+    "imageUrl": "https://th.bing.com/th/id/OIG.pfzbb.RTedASRo.Cq2iQ?pid=ImgGn",
+    "facts": {
+      "model year": "2020",
+      "material": "Malla/Sintético",
+      "gender": "Unisex",
+      "colors": ["Gris", "Azul", "Negro"],
+      "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "HokaOneOne002",
+  "name": "Hoka One One Bondi 7",
+  "shortDescription": "Máximo confort en cada paso.",
+  "description": "El Hoka One One Bondi 7 es conocido por su máximo confort en cada paso. Con una suela gruesa y una amortiguación suave, es perfecto para quienes buscan una experiencia de carrera cómoda y de apoyo.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.rVsmN3yeop1Yp44egt9y?pid=ImgGn",
+  "facts": {
+    "model year": "2020",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Blanco", "Negro", "Azul"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+}
+},
+{
+  "Id": "HokaOneOne003",
+      "name": "Hoka One One Speedgoat 4",
+      "shortDescription": "Rendimiento en terrenos difíciles.",
+      "description": "El Hoka One One Speedgoat 4 es ideal para correr en terrenos difíciles y desafiantes. Con una suela exterior de gran tracción y una construcción resistente, es una elección sólida para trail running y aventuras al aire libre.",
+      "imageUrl": "https://th.bing.com/th/id/OIG.0tglc.NuxOh4MRUMF93D?pid=ImgGn",
+      "facts": {
+        "model year": "2020",
+        "material": "Malla/Sintético",
+        "gender": "Unisex",
+        "colors": ["Rojo", "Verde", "Negro"],
+        "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+      }
+},
+{
+  "Id": "Salomon001",
+  "name": "Salomon Speedcross 5",
+  "shortDescription": "Rendimiento en terrenos difíciles.",
+  "description": "El Salomon Speedcross 5 es un zapato de trail running diseñado para rendir en terrenos difíciles y desafiantes. Con su suela de gran tracción y su construcción resistente, es una elección sólida para aquellos que disfrutan de las aventuras al aire libre.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.CuvZEsfF6HndeIOTrmdl?pid=ImgGn",
+  "facts": {
+    "model year": "2019",
+    "material": "Textil/Sintético",
+    "gender": "Unisex",
+    "colors": ["Negro", "Azul", "Verde"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Salomon002",
+  "name": "Salomon X Ultra 3",
+  "shortDescription": "Comodidad y versatilidad en senderismo.",
+  "description": "El Salomon X Ultra 3 ofrece comodidad y versatilidad para el senderismo. Su diseño ligero y su suela exterior con buen agarre lo hacen adecuado para una variedad de condiciones de senderismo y actividades al aire libre.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.VpHKbIJVC1cq9icqGRSu?pid=ImgGn",
+  "facts": {
+    "model year": "2018",
+    "material": "Cuero/Sintético",
+    "gender": "Unisex",
+    "colors": ["Gris", "Negro", "Verde"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Salomon003",
+  "name": "Salomon Sense Ride 4",
+  "shortDescription": "Comodidad en carrera de trail.",
+  "description": "El Salomon Sense Ride 4 es ideal para correr en senderos. Con una amortiguación suave y una parte superior transpirable, brinda comodidad en cada paso. Es una elección sólida para los entusiastas del trail running.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.37qeRWHp6I3ALp_Ub5R9?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Textil/Sintético",
+    "gender": "Unisex",
+    "colors": ["Azul", "Rojo", "Negro"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "AltraFootwear001",
+  "name": "Altra Escalante",
+  "shortDescription": "Comodidad y estilo para corredores.",
+  "description": "Los Altra Escalante son conocidos por su comodidad y estilo para corredores. Con su diseño ligero y su plataforma Zero Drop, proporcionan una experiencia de carrera natural. Son ideales para quienes buscan comodidad y rendimiento en sus carreras.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.jFCnXMk_jXoRkJ3qGTKw?pid=ImgGnE",
+  "facts": {
+    "model year": "2017",
+    "material": "Malla",
+    "gender": "Unisex",
+    "colors": ["Negro", "Azul", "Gris"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "AltraFootwear002",
+  "name": "Altra Lone Peak",
+  "shortDescription": "Zapato de trail versátil.",
+  "description": "Los Altra Lone Peak son zapatos de trail versátiles conocidos por su tracción y comodidad en terrenos difíciles. Con su amplia puntera y su suela MaxTrac, son ideales para aventuras en senderos y actividades al aire libre.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.AiIgMQrETtaGsXi.u.4B?pid=ImgGn",
+  "facts": {
+    "model year": "2019",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Verde", "Gris", "Negro"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "AltraFootwear003",
+  "name": "Altra Olympus",
+  "shortDescription": "Máxima amortiguación en trail running.",
+  "description": "Los Altra Olympus ofrecen máxima amortiguación para trail running. Con su suela Vibram MegaGrip y su amplia puntera, brindan comodidad y agarre en terrenos difíciles. Son ideales para corredores de larga distancia en senderos.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.a7pyuN94js0zZLrkyxTC?pid=ImgGn",
+  "facts": {
+    "model year": "2020",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Negro", "Rojo", "Azul"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "NewtonRunning001",
+  "name": "Newton Running Gravity 10",
+  "shortDescription": "Comodidad y velocidad para corredores.",
+  "description": "Los Newton Running Gravity 10 son conocidos por proporcionar comodidad y velocidad a los corredores. Con su tecnología de plataforma POP 3 y su diseño ligero, son ideales para aquellos que buscan un rendimiento excepcional en sus carreras.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.vC1vJ0QI4FSY.cBRfroe?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Rojo", "Azul", "Negro"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "NewtonRunning002",
+  "name": "Newton Running Fate 7",
+  "shortDescription": "Amortiguación y estabilidad para corredores.",
+  "description": "Los Newton Running Fate 7 ofrecen amortiguación y estabilidad a los corredores. Con su tecnología Action/Reaction y su diseño versátil, son ideales para quienes buscan una experiencia de carrera suave y apoyo.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.JzRNxr76TPNkdW24KUKW?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Blanco", "Negro", "Gris"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "NewtonRunning003",
+  "name": "Newton Running Motion 10",
+  "shortDescription": "Apoyo y comodidad en cada paso.",
+  "description": "Los Newton Running Motion 10 brindan apoyo y comodidad en cada paso. Con su tecnología de estabilidad medial y su construcción de alta calidad, son ideales para corredores que buscan una experiencia de carrera equilibrada.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.RjJYDeyNoPKhwo6n.FZC?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Negro", "Azul", "Rojo"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Brooks001",
+  "name": "Brooks Ghost 14",
+  "shortDescription": "Comodidad y versatilidad para corredores.",
+  "description": "Los Brooks Ghost 14 son conocidos por proporcionar comodidad y versatilidad a los corredores. Con su amortiguación suave y su diseño ligero, son ideales para quienes buscan un zapato versátil para entrenamiento y carreras.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.q5TFGo1uOwLqszDmMkcu?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Azul", "Negro", "Gris"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Brooks002",
+  "name": "Brooks Adrenaline GTS 22",
+  "shortDescription": "Apoyo y estabilidad para corredores.",
+  "description": "Los Brooks Adrenaline GTS 22 ofrecen apoyo y estabilidad a los corredores. Con su tecnología GuideRails y su construcción de alta calidad, son ideales para quienes buscan control de pronación y comodidad en sus carreras.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.T5NzMIDpTsYzOM9llJ9L?pid=ImgGn",
+  "facts": {
+    "model year": "2022",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Blanco", "Negro", "Verde"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Brooks003",
+  "name": "Brooks Launch 8",
+  "shortDescription": "Zapato ligero para corredores neutros.",
+  "description": "Los Brooks Launch 8 son zapatos ligeros ideales para corredores neutros. Con su diseño rápido y su amortiguación energizante, son perfectos para carreras rápidas y entrenamientos intensivos.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.hrDPq9CRqhhguvIPwcbZ?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Rojo", "Negro", "Amarillo"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Saucony001",
+  "name": "Saucony Kinvara 12",
+  "shortDescription": "Comodidad y velocidad para corredores.",
+  "description": "Los Saucony Kinvara 12 son conocidos por ofrecer comodidad y velocidad a los corredores. Con su diseño ligero y su amortiguación reactiva, son ideales para aquellos que buscan un zapato versátil para entrenamiento y carreras.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.qeS94GsD4i7ksGd56Kue?pid=ImgGn",
+  "facts": {
+    "model year": "2022",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Negro", "Azul", "Gris"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Saucony002",
+  "name": "Saucony Guide 14",
+  "shortDescription": "Apoyo y estabilidad para corredores.",
+  "description": "Los Saucony Guide 14 ofrecen apoyo y estabilidad a los corredores. Con su tecnología PWRRUN y su construcción de alta calidad, son ideales para quienes buscan control de pronación y comodidad en sus carreras.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.z.a0evbvtr99zlngBbXa?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Blanco", "Negro", "Azul"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Saucony003",
+  "name": "Saucony Peregrine 11",
+  "shortDescription": "Zapato de trail versátil.",
+  "description": "Los Saucony Peregrine 11 son zapatos de trail versátiles conocidos por su tracción y comodidad en terrenos difíciles. Con su suela PWRTRAC y su construcción resistente, son ideales para aventuras en senderos y actividades al aire libre.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.d9i97KpCcocXu.OoHnTE?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Rojo", "Negro", "Verde"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Mizuno001",
+  "name": "Mizuno Wave Rider 25",
+  "shortDescription": "Comodidad y rendimiento para corredores.",
+  "description": "Los Mizuno Wave Rider 25 son conocidos por ofrecer comodidad y rendimiento a los corredores. Con su tecnología Wave y su diseño ligero, son ideales para aquellos que buscan un zapato versátil para entrenamiento y carreras.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.BmXtTguShoqA.UN.Xlln?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Azul", "Negro", "Blanco"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Mizuno002",
+  "name": "Mizuno Wave Inspire 17",
+  "shortDescription": "Apoyo y estabilidad para corredores.",
+  "description": "Los Mizuno Wave Inspire 17 ofrecen apoyo y estabilidad a los corredores. Con su tecnología Mizuno Wave y su construcción de alta calidad, son ideales para quienes buscan control de pronación y comodidad en sus carreras.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.qXWyw6bHyzlWP3bPBLqu?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Blanco", "Negro", "Azul"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Mizuno003",
+  "name": "Mizuno Wave Hayate 7",
+  "shortDescription": "Zapato de trail versátil.",
+  "description": "Los Mizuno Wave Hayate 7 son zapatos de trail versátiles conocidos por su tracción y comodidad en terrenos difíciles. Con su suela Michelin y su construcción resistente, son ideales para aventuras en senderos y actividades al aire libre.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.._7Z9osXCYjMA6.RaOpM?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Rojo", "Negro", "Verde"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Merrell001",
+  "name": "Merrell Moab 2 Ventilator",
+  "shortDescription": "Zapato versátil para senderismo.",
+  "description": "Los Merrell Moab 2 Ventilator son conocidos por su versatilidad y comodidad en actividades al aire libre. Con su diseño transpirable y su suela Vibram, son ideales para caminatas y senderismo en terrenos variados.",
+  "imageUrl": "https://th.bing.com/th/id/OIG..H7SnjoSQl2V4nzHX03w?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Cuero/Sintético",
+    "gender": "Unisex",
+    "colors": ["Gris", "Marrón", "Verde"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Merrell002",
+  "name": "Merrell All Out Blaze Aero Sport",
+  "shortDescription": "Zapato de agua y senderismo.",
+  "description": "Los Merrell All Out Blaze Aero Sport son zapatos versátiles ideales para actividades acuáticas y senderismo. Con su diseño de secado rápido y su suela con tracción, son perfectos para aventuras en agua y terrenos húmedos.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.bkE0QajWCsq5WtRParzr?pid=ImgGn",
+  "facts": {
+    "model year": "2021",
+    "material": "Malla/Sintético",
+    "gender": "Unisex",
+    "colors": ["Azul", "Negro", "Verde"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+{
+  "Id": "Merrell003",
+  "name": "Merrell Jungle Moc",
+  "shortDescription": "Comodidad casual en la ciudad o en la naturaleza.",
+  "description": "Los Merrell Jungle Moc ofrecen comodidad casual para uso en la ciudad o en la naturaleza. Con su diseño deslizable y su suela antideslizante, son ideales para el uso diario y actividades al aire libre.",
+  "imageUrl": "https://th.bing.com/th/id/OIG.OHXYNdmsYSnqDq9..DsU?pid=ImgGn",
+  "facts": {
+    "model year": "2020",
+    "material": "Cuero/Sintético",
+    "gender": "Unisex",
+    "colors": ["Negro", "Marrón", "Gris"],
+    "sizes": ["US 6", "US 7", "US 8", "US 9", "US 10", "US 11"]
+  }
+},
+
+
+
+
+
+
+
+
+
   //ejemplo
 /*  {
     "id": "ada-lovelace",
