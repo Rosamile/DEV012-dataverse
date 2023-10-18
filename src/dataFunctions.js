64ac2328c0b1d425d6e66dd1e63f8b88494a85d9
Emma Linares <elinares235@gmail.com>
2023-10-18 11:48:36 -0300

    Updates

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -3,17 +3,14 @@ import data from "./data/dataset.js";
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 export const example = () => {
   return 'example';
-};
-
+}
 export const pintar = (data) => {console.log(data)
   for (let i =0;i<data.length;i++){
     return 'pintar';
   }
-};
-
+}
 function tipodeZapatillas(zapatillas) {
 const zapatilla = document.createElement()
-}
-
+};
 export const filterAllBrands = data.filter(data => data.brand);
-console.log(filterAllBrands);
+console.log(filterAllBrands)
