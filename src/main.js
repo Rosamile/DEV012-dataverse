452f95384585197ecebd082b07a13457773c52e5
Emma Linares <elinares235@gmail.com>
2023-10-23 10:59:55 -0300

    Cambios en dataset, datafutnion, index, main y view

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
-import { pintar, orderByAlphabetical} from './dataFunctions.js';
+import {pintar, orderByAlphabetical, filterDataByBrand} from './dataFunctions.js';
 import { renderItems } from './view.js';
 import data from './data/dataset.js';
 
+// Evento para invocar la data ordenada
 const ordenar = document.querySelector("select[name='ordenar']");
 ordenar.addEventListener("change", () => {
   const sortedData = orderByAlphabetical(data, item => item.name);
-  zapatilla.innerHTML=renderItems(sortedData); // Volver a renderizar con los datos ordenados
+  zapatilla.innerHTML = renderItems(sortedData); // Vuelve a renderizar con los datos ordenados
 })
 
 const zapatilla =  document.getElementById("zapatillas")
 zapatilla.innerHTML = renderItems(data); 
 
-//const datafiltrada = 
+// Evento para invocar la data filtrada
+const filter = document.querySelector("select[name='brand']");
+filter.addEventListener("change", () => {
+  const selectedBrand = filter.value; // Obtener el valor seleccionado en el select
+  const filteredData = filterDataByBrand(data, 'brand', selectedBrand); // Pasa el valor seleccionado como tercer argumento
+  zapatilla.innerHTML = renderItems(filteredData);
+})
+
+// Evento para limpiar la data filtrada y ordenada
+
+
