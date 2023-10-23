495571d7b957b975952c333e972ed8cd01017124
Rosa Sarmiento <rosamilets@gmail.com>
2023-10-23 09:23:12 -0500

    Se modifica el ordenamiento con su respectivo evento

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { pintar, orderByAlphabetical} from './dataFunctions.js';
 import { renderItems } from './view.js';
 import data from './data/dataset.js';
 
+
 const ordenar = document.querySelector("select[name='ordenar']");
 ordenar.addEventListener("change", () => {
-  const sortedData = orderByAlphabetical(data, item => item.name);
-  zapatilla.innerHTML=renderItems(sortedData); // Volver a renderizar con los datos ordenados
+  const selectedOrder = ordenar.value ;
+  const sortedData = orderByAlphabetical(data, item => item.name, selectedOrder);
+  zapatilla.innerHTML = renderItems(sortedData); // Volver a renderizar con los datos ordenados
 })
 
 const zapatilla =  document.getElementById("zapatillas")
