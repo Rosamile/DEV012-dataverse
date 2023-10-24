21ef4709446201133391b3fba13d38cb8a6c6f12
Emma Linares <elinares235@gmail.com>
2023-10-24 16:38:50 -0300

    Cambios directos en rama main

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import { orderByAlphabetical,filterDataByBrand} from './dataFunctions.js';
+import { tipoDeZapatillas, orderByAlphabetical,filterDataByBrand} from './dataFunctions.js';
 import { renderItems } from './view.js';
 import data from './data/dataset.js';
 
+const zapatillaDiv = document.createElement(data);
+zapatillaDiv.textContent = "Contenido de la zapatilla"; // Aquí puedes establecer el contenido de la zapatilla
 
-// Renderizar la data
+const zapatillasContainer = document.getElementById("zapatillaDiv");
+zapatillasContainer.appendChild(zapatillaDiv);
 
-const zapatilla =  document.getElementById("zapatillas")
-zapatilla.innerHTML = renderItems(data); 
 
 // Evento para Ordenar la data por modelo
 
@@ -14,7 +15,7 @@ const ordenar = document.querySelector("select[name='ordenar']");
 ordenar.addEventListener("change", () => {
   const selectedOrder = ordenar.value; 
   const sortedData = orderByAlphabetical(data, item => item.name, selectedOrder);
-  zapatilla.innerHTML = renderItems(sortedData);
+  zapatillaDiv.innerHTML = renderItems(sortedData);
 });
 
 // Evento para limpiar los filtros
@@ -23,7 +24,7 @@ const limpiar = document.getElementById('button-clear');
 limpiar.addEventListener("click", () => {
   ordenar.value= "todos";
   filter.value="allbrands";
-  zapatilla.innerHTML = renderItems(data); 
+  zapatillaDiv.innerHTML = renderItems(data); 
 });
 
 // Evento para invocar la data filtrada
@@ -32,6 +33,6 @@ const filter = document.querySelector("select[name='brand']");
 filter.addEventListener("change", () => {
   const selectedBrand = filter.value; // Obtener el valor seleccionado en el select
   const filteredData = filterDataByBrand(data, 'brand', selectedBrand); // Pasa el valor seleccionado como tercer argumento
-  zapatilla.innerHTML = renderItems(filteredData); 
+  zapatillaDiv.innerHTML = renderItems(filteredData); 
 });
 
