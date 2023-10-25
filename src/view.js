06386467cb6e3407c450acee384c6d296356b2da
Rosa Sarmiento <rosamilets@gmail.com>
2023-10-25 11:49:10 -0500

    corrección data set y errores en espacios

--- src/view.js
+++ src/view.js
@@ -5,32 +5,22 @@ import data from "./data/dataset.js";
 export const renderItems = (data) => {
   let cajadevariables = "<ul>";
   for (let i = 0; i < data.length; i++) {
-
     const htmlR = `
-     <li class="description" itemscope itemtype="Estilo Athleisure"></li>
-    <li class="description" itemscope itemtype="Estilo Athleisure"></li>
-                            <li class="contenedor">
+                <li class="contenedor" itemscope itemtype="Estilo Athleisure">
                                 <dl class="tarjeta">
                                 <img src="${data[i].imageUrl}" alt="Imagenes de zapatos"/>
                                     <dt><strong>Marca:</strong></dt>
                                     <dd itemprop="brand">${data[i].brand}</dd>
-                                    <dt><strong>Modelo:</strong></dt>
-                                    <dd itemprop="name">${data[i].name}</dd>
-                                    <dt><strong>Descripción:</strong></dt>
-                                    <dd itemprop="description">${data[i].description}</dd>
-                                    <dt><strong>Año del modelo:</strong></dt>
-                                    <dd itemprop="modelyear">${data[i].facts.modelyear}</dd>
-                                    <dt><strong>Material:</strong></dt>
-                                    <dd itemprop="material">${data[i].facts.material}</dd>
-                                    <dt><strong>Género:</strong></dt>
-                                    <dd itemprop="gender">${data[i].facts.gender}</dd>
-                                    <dt><strong>Colores:</strong></dt>
-                                    <dd itemprop="colors">${data[i].facts.colors}</dd>
-                                    <dt><strong>Tallas:</strong></dt>
-                                    <dd itemprop="sizes">${data[i].facts.sizes}</dd>
+                                    <dt><strong>Modelo:</strong></dt> <dd itemprop="name">${data[i].name}</dd>
+                                    <dt><strong>Descripción:</strong></dt><dd itemprop="description">${data[i].description}</dd>
+                                    <dt><strong>Año del modelo:</strong></dt><dd itemprop="modelyear">${data[i].facts.modelyear}</dd>
+                                    <dt><strong>Material:</strong></dt><dd itemprop="material">${data[i].facts.material}</dd>
+                                    <dt><strong>Género:</strong></dt><dd itemprop="gender">${data[i].facts.gender}</dd>
+                                    <dt><strong>Colores:</strong></dt><dd itemprop="colors">${data[i].facts.colors}</dd>
+                                    <dt><strong>Tallas:</strong></dt><dd itemprop="sizes">${data[i].facts.sizes}</dd>
                                     </dl>
-                                    </li>
-                               </li>`  
+                  </li>
+                               </li>`
     cajadevariables += htmlR;
   }
   return cajadevariables +="</ul>"
